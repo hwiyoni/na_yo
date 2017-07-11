ae66658545475ef6a2195d37cb55135d6f6d2d82
hwiyoni <gnldus28@unist.ac.kr>
2017-07-11 15:57:56 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = ['',
+urlpatterns = [
                url(r'^admin/', include(admin.site.urls)),
                url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
                url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
